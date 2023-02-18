b054759ef6b0004489520871f93681ac87adb9c5
LAPTOP-3HT9DOHB\Chang <ch96963@gmail.com>
2023-02-18 15:12:29 +0800

    feat : 動態牆瀑布流效果

--- src/page/Home/components/OtherUser.tsx
+++ src/page/Home/components/OtherUser.tsx
@@ -6,9 +6,11 @@ import { ReactComponent as LikeActive } from "../../../img/otherUser/likeActive.
 import { useSelector, useDispatch } from "react-redux";
 import * as Styles from "../styles";
 import { RootState } from "../../../reducers";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { AddOtherUserDataLike } from "../../../reducers/otherUserData";
 import { GlobalClientImg } from "../../../styles/GlobalStyle";
+import InfiniteScroll from "react-infinite-scroll-component";
+import ReactLoading from "react-loading";
 
 interface otherUserDataProps {
   id: string;
@@ -33,72 +35,107 @@ const OtherUser = () => {
     otherUserDataProps[]
   >([]);
 
+  const [dataPageNumber, setDataPageNumber] = useState<number>(4);
+  const [hasMore, setHasMore] = useState<boolean>(true);
+
+  const fetchMoreData = () => {
+    // mock APi call
+    if (sortIdOtherUserData.length < otherUserData.length) {
+      setTimeout(() => {
+        setDataPageNumber(dataPageNumber + 2);
+      }, 1500);
+    } else {
+      setHasMore(false);
+    }
+  };
+
   useEffect(() => {
     setSortIdOtherUserData(
-      otherUserData
-        .slice()
-        .sort((a: otherUserDataProps, b: otherUserDataProps) => {
-          return Number(b.id) - Number(a.id);
-        })
+      otherUserData.slice(0, dataPageNumber)
+      // .sort((a: otherUserDataProps, b: otherUserDataProps) => {
+      //   return Number(b.id) - Number(a.id);
+      // })
     );
-  }, [otherUserData]);
+  }, [otherUserData, dataPageNumber]);
 
   return (
     <Styles.OtherUser>
-      {sortIdOtherUserData?.map((item) => {
-        return (
-          <div className="other-user-content" key={item.id}>
-            <GlobalClientImg
-              src="https://picsum.photos/50/50?grayscale"
-              alt=""
-              className="client-data-img"
-              Location={"otherUser"}
+      <InfiniteScroll
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              margin: "12px 0 0 0",
+            }}
+          >
+            <ReactLoading
+              type={"spokes"}
+              color="#1d9bf0"
+              width="25px"
+              height="25px"
             />
+          </div>
+        }
+        dataLength={sortIdOtherUserData.length}
+        style={{ overflow: "hidden" }}
+      >
+        {sortIdOtherUserData?.map((item) => {
+          return (
+            <div className="other-user-content" key={item.id}>
+              <GlobalClientImg
+                src="https://picsum.photos/50/50?grayscale"
+                alt=""
+                className="client-data-img"
+                Location={"otherUser"}
+              />
+              <div className="other-data-container">
+                <div className="other-user-block">
+                  <div className="other-user-name">{item.userName}</div>
+                  <div className="other-user-account">
+                    {item.userSerialNumber}
+                  </div>
+                  ·<div className="other-user-date">31m</div>
+                </div>
 
-            <div className="other-data-container">
-              <div className="other-user-block">
-                <div className="other-user-name">{item.userName}</div>
-                <div className="other-user-account">
-                  {item.userSerialNumber}
+                <div className="other-user-text">{item.text}</div>
+                <div className="other-user-image">
+                  <img
+                    src={`https://picsum.photos/500/370/?blur=${item.id}`}
+                    alt=""
+                  ></img>
                 </div>
-                ·<div className="other-user-date">31m</div>
-              </div>
-
-              <div className="other-user-text">{item.text}</div>
-              <div className="other-user-image">
-                <img
-                  src={`https://picsum.photos/500/370/?blur=${item.id}`}
-                  alt=""
-                ></img>
-              </div>
-              <div className="other-user-icon">
-                <div className="other-user-icon-item">
-                  <Message />
-                  {item.message}
-                </div>
-                <div className="other-user-icon-item transfer">
-                  <Transfer /> {item.transfer}
-                </div>
-                <div className="other-user-icon-item">
-                  <View />
-                  {item.view}
-                </div>
-                <div
-                  className={`other-user-icon-item like ${
-                    item.like.userClick ? "active" : ""
-                  }`}
-                  onClick={() => {
-                    dispatch(AddOtherUserDataLike(item.id));
-                  }}
-                >
-                  {item.like.userClick ? <LikeActive /> : <Like />}
-                  {item.like.number}
+                <div className="other-user-icon">
+                  <div className="other-user-icon-item">
+                    <Message />
+                    {item.message}
+                  </div>
+                  <div className="other-user-icon-item transfer">
+                    <Transfer /> {item.transfer}
+                  </div>
+                  <div className="other-user-icon-item">
+                    <View />
+                    {item.view}
+                  </div>
+                  <div
+                    className={`other-user-icon-item like ${
+                      item.like.userClick ? "active" : ""
+                    }`}
+                    onClick={() => {
+                      dispatch(AddOtherUserDataLike(item.id));
+                    }}
+                  >
+                    {item.like.userClick ? <LikeActive /> : <Like />}
+                    {item.like.number}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </InfiniteScroll>
     </Styles.OtherUser>
   );
 };
