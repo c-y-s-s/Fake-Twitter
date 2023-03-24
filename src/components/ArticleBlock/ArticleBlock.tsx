bad7c102fc410b07000c8f34c06a25131edc04c9
LAPTOP-3HT9DOHB\Chang <ch96963@gmail.com>
2023-03-25 00:04:40 +0800

    fix : 用戶名稱修改時,發布過的文章名稱不會同布更新

--- src/components/ArticleBlock/ArticleBlock.tsx
+++ src/components/ArticleBlock/ArticleBlock.tsx
@@ -55,6 +55,7 @@ const ArticleBlock: FC<ArticleBlockProps> = ({
   const [articleData, setArticleData] = useState<articleDataProps[]>([]);
   const [dataPageNumber, setDataPageNumber] = useState<number>(4);
   const [hasMore, setHasMore] = useState<boolean>(true);
+  const [currentUserName, setCurrentUserName] = useState<any>({});
   const uid = firebase?.auth()?.currentUser?.uid;
 
   const fetchMoreData = () => {
@@ -68,13 +69,41 @@ const ArticleBlock: FC<ArticleBlockProps> = ({
   };
 
   useEffect(() => {
-    setSortIdOtherUserData(articleData.slice(0, dataPageNumber));
-  }, [dataPageNumber, articleData]);
+    const resultData = articleData.map((item: articleDataProps) => {
+      if (item?.author?.email === currentUserName?.mail) {
+        return {
+          ...item,
+          resultName: currentUserName.name,
+        };
+      }
+    });
+    setSortIdOtherUserData(resultData.slice(0, dataPageNumber));
+  }, [
+    dataPageNumber,
+    articleData,
+    currentUserName?.mail,
+    currentUserName.name,
+  ]);
 
   // 拿 database data
   // 判斷哪邊使用到這個元件去各別拿取要的資料
   useEffect(() => {
     if (firebase?.auth()?.currentUser?.uid === null) return;
+
+    // userData
+    firebase
+      .firestore()
+      .collection("users")
+      .get()
+      .then((res) => {
+        res.docs.map((item) => {
+          setCurrentUserName({
+            mail: item?.data().mail,
+            name: item?.data().name,
+          });
+        });
+      });
+
     switch (useBlocks) {
       case "home":
         firebase
@@ -93,11 +122,13 @@ const ArticleBlock: FC<ArticleBlockProps> = ({
       case "profile":
         let query: any;
         if (tabListSwitch === "Article") {
+          console.log("a");
           query = firebase
             ?.firestore()
             ?.collection("posts_article")
             ?.where("author.uid", "==", firebase?.auth()?.currentUser?.uid);
         } else {
+          console.log("b");
           query = firebase
             ?.firestore()
             ?.collection("posts_article")
@@ -112,6 +143,7 @@ const ArticleBlock: FC<ArticleBlockProps> = ({
             const id = doc?.id;
             return { id, ...doc?.data() };
           });
+
           if (setArticlesTotalNumber) setArticlesTotalNumber(data.length);
           setArticleData(data as []);
         });
@@ -120,7 +152,7 @@ const ArticleBlock: FC<ArticleBlockProps> = ({
       default:
         break;
     }
-  }, [tabListSwitch, useBlocks]);
+  }, [setArticlesTotalNumber, tabListSwitch, useBlocks]);
 
   return (
     <Styles.OtherUser>
@@ -151,64 +183,79 @@ const ArticleBlock: FC<ArticleBlockProps> = ({
           if (uid) {
             isItemLikeL = item?.likeBy?.includes(uid);
           }
-          return (
-            <div className="other-user-content" key={item.id}>
-              <GlobalClientImg
-                src={userData.photoURL}
-                alt=""
-                className="client-data-img"
-                Location={"otherUser"}
-              />
-              <div className="other-data-container">
-                <div className="other-user-block">
-                  <div className="other-user-name">
-                    {item?.author?.userName}
-                  </div>
-                  <div className="other-user-account">
-                    @{item?.author?.membershipNumber}
-                  </div>
-                  ·<div className="other-user-date">31m</div>
-                </div>
 
-                <div className="other-user-text">{item.text}</div>
-                {item.imageUrl ? (
-                  <div className="other-user-image">
-                    <img src={item.imageUrl} alt=""></img>
+          // 計算發布時間距離現在時間多久
+          let resultTime: string = "";
+          let resultSecond: number =
+            (new Date().getTime() - item?.createdAt?.seconds * 1000) / 1000;
+
+          if (resultSecond < 60) {
+            resultTime = resultSecond.toFixed(0) + "秒前發佈";
+          } else if (resultSecond / 60 < 60) {
+            resultTime = (resultSecond / 60).toFixed(0) + "分鐘前發佈";
+          } else if (resultSecond / 60 > 60 && resultSecond / 60 < 1800) {
+            resultTime = (resultSecond / 60 / 60).toFixed(0) + "小時前發佈";
+          } else if (resultSecond / 60 > 1800) {
+            resultTime = resultSecond / 60 / 24 + "小時前發佈";
+          }
+
+          return (
+            item?.id && (
+              <div className="other-user-content" key={item?.id}>
+                <GlobalClientImg
+                  src={userData.photoURL}
+                  alt=""
+                  className="client-data-img"
+                  Location={"otherUser"}
+                />
+                <div className="other-data-container">
+                  <div className="other-user-block">
+                    <div className="other-user-name">{item?.resultName}</div>
+                    <div className="other-user-account">
+                      @{item?.author?.membershipNumber}
+                    </div>
+                    ·<div className="other-user-date">{resultTime}</div>
                   </div>
-                ) : (
-                  <></>
-                )}
-                <div className="other-user-icon">
-                  <div className="other-user-icon-item">
-                    <Message />
-                    12
-                  </div>
-                  <div
-                    className="other-user-icon-item transfer"
-                    onClick={() => {
-                      UseToggleCollected(item.id);
-                    }}
-                  >
-                    <Transfer />
-                    {item?.transferBy?.length ? item.transferBy?.length : 0}
-                  </div>
-                  <div className="other-user-icon-item">
-                    <View /> 12
-                  </div>
-                  <div
-                    className={`other-user-icon-item like ${
-                      isItemLikeL ? "active" : ""
-                    }`}
-                    onClick={() => {
-                      UseToggleLike(item.id);
-                    }}
-                  >
-                    {isItemLikeL ? <LikeActive /> : <Like />}
-                    {item?.likeBy?.length ? item.likeBy?.length : 0}
+                  <div className="other-user-text">{item?.text}</div>
+                  {item?.imageUrl ? (
+                    <div className="other-user-image">
+                      <img src={item?.imageUrl} alt=""></img>
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                  <div className="other-user-icon">
+                    <div className="other-user-icon-item">
+                      <Message />
+                      12
+                    </div>
+                    <div
+                      className="other-user-icon-item transfer"
+                      onClick={() => {
+                        UseToggleCollected(item?.id);
+                      }}
+                    >
+                      <Transfer />
+                      {item?.transferBy?.length ? item.transferBy?.length : 0}
+                    </div>
+                    <div className="other-user-icon-item">
+                      <View /> 12
+                    </div>
+                    <div
+                      className={`other-user-icon-item like ${
+                        isItemLikeL ? "active" : ""
+                      }`}
+                      onClick={() => {
+                        UseToggleLike(item?.id);
+                      }}
+                    >
+                      {isItemLikeL ? <LikeActive /> : <Like />}
+                      {item?.likeBy?.length ? item.likeBy?.length : 0}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            )
           );
         })}
       </InfiniteScroll>
